e7f9771c1e738891c85817c4b4775f716e04c93b
Or Hasson <hassonor@gmail.com>
2023-10-31 15:40:23 +0200

    adding header and userInput elements

---
+++ react-complete-guide/react-essentials-project/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header/Header.jsx";
+import UserInput from "./components/UserInput/UserInput.jsx";
+
+function App() {
+    return (
+        <>
+            <Header/>
+            <UserInput/>
+        </>
+    )
+}
+
+export default App
