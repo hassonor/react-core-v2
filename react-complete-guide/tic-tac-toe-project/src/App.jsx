25587eb66009554d1617315290b93f9b5685c3c3
Or Hasson <hassonor@gmail.com>
2023-10-31 12:25:22 +0200

    adding comibination win checking logic

--- react-complete-guide/tic-tac-toe-project/src/App.jsx
+++ react-complete-guide/tic-tac-toe-project/src/App.jsx
@@ -2,6 +2,13 @@ import Player from "./components/Player/Player.jsx";
 import GameBoard from "./components/GameBoard/GameBoard.jsx";
 import {useState} from "react";
 import Log from "./components/Log/Log.jsx";
+import {WINNING_COMBINATIONS} from "./winning-combinations.js";
+
+const initialGameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+]
 
 
 function deriveActivePlayer(gameTurns) {
@@ -17,6 +24,27 @@ function App() {
     const [gameTurns, setGameTurns] = useState([]);
 
     const activePlayer = deriveActivePlayer(gameTurns);
+
+    let gameBoard = initialGameBoard;
+
+    for (const turn of gameTurns) {
+        const {square, player} = turn
+        const {row, col} = square;
+
+        gameBoard[row][col] = player;
+    }
+
+    let winner;
+
+    for (const combination of WINNING_COMBINATIONS) {
+        const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+        const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+        const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
+
+        if (firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol) {
+            winner = firstSquareSymbol;
+        }
+    }
 
 
     function handleSelectSquare(rowIndex, colIndex) {
@@ -35,7 +63,8 @@ function App() {
                     <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
                     <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
                 </ol>
-                <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns}/>
+                {winner && <p> You won, {winner}!</p>}
+                <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard}/>
             </div>
             <Log turns={gameTurns}/>
         </main>
--- react-complete-guide/tic-tac-toe-project/src/components/GameBoard/GameBoard.jsx
+++ react-complete-guide/tic-tac-toe-project/src/components/GameBoard/GameBoard.jsx
@@ -1,25 +1,12 @@
 import {useState} from "react";
 
-const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-]
 
-export default function GameBoard({onSelectSquare, turns}) {
-    let gameBoard = initialGameBoard;
-
-    for (const turn of turns) {
-        const {square, player} = turn
-        const {row, col} = square;
-
-        gameBoard[row][col] = player;
-    }
-
+export default function GameBoard({onSelectSquare, board}) {
     return <ol id="game-board">
-        {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
+        {board.map((row, rowIndex) => <li key={rowIndex}>
             <ol>{row.map((playerSymbol, colIndex) => <li key={colIndex}>
-                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)}
+                        disabled={playerSymbol !== null}>
                     {playerSymbol}
                 </button>
             </li>)}</ol>
