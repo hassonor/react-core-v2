ce6da9bce1eb3124f8ddbd10019689fc8162b453
Or Hasson <hassonor@gmail.com>
2023-11-02 09:26:46 +0200

    show example for resuable components

--- react-complete-guide/styling-on-react-project/src/components/AuthInputs.jsx
+++ react-complete-guide/styling-on-react-project/src/components/AuthInputs.jsx
@@ -1,5 +1,7 @@
 import {useState} from 'react';
 import {styled} from 'styled-components';
+import Button from "./Button.jsx";
+import CustomInput from "./Input.jsx";
 
 
 const ControlContainer = styled.div`
@@ -7,42 +9,6 @@ const ControlContainer = styled.div`
   flex-direction: column;
   gap: 0.5rem;
   margin-bottom: 1.5rem;
-`
-
-const Label = styled.label`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color: ${({$invalid}) => $invalid ? '#f87171' : '#6b7280'};
-`
-
-const Input = styled.input`
-  width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  background-color: ${({$invalid}) => $invalid ? '#fed2d2' : '#d1d5db'};
-  color: ${({$invalid}) => $invalid ? '#ef4444' : '#374151'};
-  border: 1px solid ${({$invalid}) => $invalid ? '#f73f3f' : 'transparent'};;
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`
-
-const Button = styled.button`
-  padding: 1rem 2rem;
-  font-weight: 600;
-  text-transform: uppercase;
-  border-radius: 0.25rem;
-  color: #1f2937;
-  background-color: #f0b322;
-  border-radius: 6px;
-  border: none;
-
-  &:hover {
-    background-color: #f0920e;
-  }
 `
 
 const TextButton = styled.button`
@@ -53,7 +19,6 @@ const TextButton = styled.button`
     color: #f0920e;
   }
 `
-
 
 export default function AuthInputs() {
     const [enteredEmail, setEnteredEmail] = useState('');
@@ -78,24 +43,20 @@ export default function AuthInputs() {
     return (
         <div id="auth-inputs">
             <ControlContainer>
-                <p>
-                    <Label $invalid={emailNotValid}>Email</Label>
-                    <Input
-                        type="email"
-                        $invalid={emailNotValid}
-                        onChange={(event) => handleInputChange('email', event.target.value)}
-                    />
-                </p>
-                <p>
-                    <Label $invalid={passwordNotValid}>Password</Label>
-                    <Input
-                        type="password"
-                        $invalid={passwordNotValid}
-                        onChange={(event) =>
-                            handleInputChange('password', event.target.value)
-                        }
-                    />
-                </p>
+                <CustomInput
+                    type="email"
+                    label="Email"
+                    invalid={emailNotValid}
+                    onChange={(event) => handleInputChange('email', event.target.value)}
+                />
+                <CustomInput
+                    type="password"
+                    label="Password"
+                    invalid={passwordNotValid}
+                    onChange={(event) =>
+                        handleInputChange('password', event.target.value)
+                    }
+                />
             </ControlContainer>
             <div className="actions">
                 <TextButton>
