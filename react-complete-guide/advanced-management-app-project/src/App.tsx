52992a6a672ab7d8d734b53d0fd2da0d80581d69
Or Hasson <hassonor@gmail.com>
2023-11-06 08:54:16 +0200

    move context logic to context provider

--- react-complete-guide/advanced-management-app-project/src/App.tsx
+++ react-complete-guide/advanced-management-app-project/src/App.tsx
@@ -4,80 +4,11 @@ import Shop from './components/Shop';
 import { DUMMY_PRODUCTS } from './dummy-products';
 import { ProductData, ShoppingCart } from "./interfaces";
 import Product from "./components/Product";
-import { CartContext } from "./store/shopping-cart-context";
+import CartContextProvider, { CartContext } from "./store/shopping-cart-context";
 
 function App() {
-    const [shoppingCart, setShoppingCart] = useState<ShoppingCart>({
-        items: [],
-    });
-
-    function handleAddItemToCart(id: string): void {
-        setShoppingCart((prevShoppingCart) => {
-            const updatedItems = [...prevShoppingCart.items];
-
-            const existingCartItemIndex = updatedItems.findIndex(
-                (cartItem) => cartItem.id === id
-            );
-            const existingCartItem = updatedItems[existingCartItemIndex];
-
-            if (existingCartItem) {
-                const updatedItem = {
-                    ...existingCartItem,
-                    quantity: existingCartItem.quantity + 1,
-                };
-                updatedItems[existingCartItemIndex] = updatedItem;
-            } else {
-                const product = DUMMY_PRODUCTS.find((product) => product.id === id);
-                if (product) {
-                    updatedItems.push({
-                        id: id,
-                        name: product.title,
-                        price: product.price,
-                        quantity: 1,
-                    });
-                }
-            }
-
-            return {
-                items: updatedItems,
-            };
-        });
-    }
-
-    function handleUpdateCartItemQuantity(productId: string, amount: number): void {
-        setShoppingCart((prevShoppingCart) => {
-            const updatedItems = [...prevShoppingCart.items];
-            const updatedItemIndex = updatedItems.findIndex(
-                (item) => item.id === productId
-            );
-
-            if (updatedItemIndex < 0) return prevShoppingCart; // or handle the error
-
-            const updatedItem = {
-                ...updatedItems[updatedItemIndex],
-                quantity: updatedItems[updatedItemIndex].quantity + amount,
-            };
-
-            if (updatedItem.quantity <= 0) {
-                updatedItems.splice(updatedItemIndex, 1);
-            } else {
-                updatedItems[updatedItemIndex] = updatedItem;
-            }
-
-            return {
-                items: updatedItems,
-            };
-        });
-    }
-
-    const ctxValue = {
-        items: shoppingCart.items,
-        addItemToCart: handleAddItemToCart,
-        updateCartItemQuantity: handleUpdateCartItemQuantity
-    }
-
     return (
-        <CartContext.Provider value={ctxValue}>
+        <CartContextProvider>
             <Header/>
             <Shop>
                 {DUMMY_PRODUCTS.map((product: ProductData) => (
@@ -87,7 +18,7 @@ function App() {
                 ))}
             </Shop>
 
-        </CartContext.Provider>
+        </CartContextProvider>
     );
 }
 
--- react-complete-guide/advanced-management-app-project/src/store/shopping-cart-context.tsx
+++ react-complete-guide/advanced-management-app-project/src/store/shopping-cart-context.tsx
@@ -1,5 +1,7 @@
-import { createContext } from 'react';
-import { CartItem } from "../interfaces";
+import { createContext, ReactNode, useState } from 'react';
+import { CartItem, ShoppingCart } from "../interfaces";
+import { DUMMY_PRODUCTS } from "../dummy-products";
+import Cart from "../components/Cart";
 
 
 interface CartContextType {
@@ -15,5 +17,84 @@ export const CartContext = createContext<CartContextType>({
 
     }
 });
+
+interface CartContextProviderProps {
+    children: ReactNode;
+}
+
+export default function CartContextProvider({children}: CartContextProviderProps) {
+    const [shoppingCart, setShoppingCart] = useState<ShoppingCart>({
+        items: [],
+    });
+
+    function handleAddItemToCart(id: string): void {
+        setShoppingCart((prevShoppingCart) => {
+            const updatedItems = [...prevShoppingCart.items];
+
+            const existingCartItemIndex = updatedItems.findIndex(
+                (cartItem) => cartItem.id === id
+            );
+            const existingCartItem = updatedItems[existingCartItemIndex];
+
+            if (existingCartItem) {
+                const updatedItem = {
+                    ...existingCartItem,
+                    quantity: existingCartItem.quantity + 1,
+                };
+                updatedItems[existingCartItemIndex] = updatedItem;
+            } else {
+                const product = DUMMY_PRODUCTS.find((product) => product.id === id);
+                if (product) {
+                    updatedItems.push({
+                        id: id,
+                        name: product.title,
+                        price: product.price,
+                        quantity: 1,
+                    });
+                }
+            }
+
+            return {
+                items: updatedItems,
+            };
+        });
+    }
+
+    function handleUpdateCartItemQuantity(productId: string, amount: number): void {
+        setShoppingCart((prevShoppingCart) => {
+            const updatedItems = [...prevShoppingCart.items];
+            const updatedItemIndex = updatedItems.findIndex(
+                (item) => item.id === productId
+            );
+
+            if (updatedItemIndex < 0) return prevShoppingCart; // or handle the error
+
+            const updatedItem = {
+                ...updatedItems[updatedItemIndex],
+                quantity: updatedItems[updatedItemIndex].quantity + amount,
+            };
+
+            if (updatedItem.quantity <= 0) {
+                updatedItems.splice(updatedItemIndex, 1);
+            } else {
+                updatedItems[updatedItemIndex] = updatedItem;
+            }
+
+            return {
+                items: updatedItems,
+            };
+        });
+    }
+
+    const ctxValue = {
+        items: shoppingCart.items,
+        addItemToCart: handleAddItemToCart,
+        updateCartItemQuantity: handleUpdateCartItemQuantity
+    }
+
+    return <CartContext.Provider value={ctxValue}>
+        {children}
+    </CartContext.Provider>
+}
 
 
